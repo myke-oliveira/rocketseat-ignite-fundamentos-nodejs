0316e05832d16238072b2e6ca02c7f79a115fde7
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-23 18:28:06 -0300

    ✨ NEW FEATURE: all HTTP verbs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,41 @@ app.get("/courses", (request, response) => {
     "Curso 1",
     "Curso 2",
     "Curso 3"
+  ]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Curso 7",
+    "Curso 3",
+    "Curso 4"
+  ])
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Curso 7",
+    "Curso 4"
   ])
 });
 
